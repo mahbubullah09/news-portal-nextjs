65b2f79b58024312bf2169564056f375b3d610ad
mahbubullah09 <mahbubullahpathan@gmail.com>
2024-04-29 21:50:03 +0600

    landing page dynamic

--- src/components/ui/LatestNews/LatestNews.js
+++ src/components/ui/LatestNews/LatestNews.js
@@ -40,7 +40,12 @@ const LatestNews = async () => {
                 </Link>
             </Box>
 
-            <Box sx={{ width: '100%' }}>
+            <Box sx={{ width: '100%' ,
+            "& img":{
+                width: "100%",
+                height: "200px"
+
+            } }}>
                 <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                     <Grid item xs={6}>
                         <Link href={`${data[1].category}/${data[1]._id}`}>
@@ -93,54 +98,54 @@ const LatestNews = async () => {
                         </Link>
                     </Grid>
                     <Grid item xs={6}>
-                    <Link href={`${data[4].category}/${data[4]._id}`}>
-              <Card >
-                    <CardActionArea>
-                        <CardMedia>
-                            <Image src={data[4].thumbnail_url} alt='news' width={800} height={500} />
-                        </CardMedia>
-                        <CardContent>
-                            <p>
-                                <span className=' bg-red-500 rounded  py-1 px-2 text-white'> {data[0].category}</span>
-                            </p>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {data[4].title}
-                            </Typography>
-                            <Typography gutterBottom >
-                            By {data[4].author.name} - {data[4].author.published_date}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                {data[4].details.length>200? data[4].details.slice(0,300)+"..." : data[0].details}
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-              </Link>
+                        <Link href={`${data[4].category}/${data[4]._id}`}>
+                            <Card >
+                                <CardActionArea>
+                                    <CardMedia>
+                                        <Image src={data[4].thumbnail_url} alt='news' width={800} height={500} />
+                                    </CardMedia>
+                                    <CardContent>
+                                        <p>
+                                            <span className=' bg-red-500 rounded  py-1 px-2 text-white'> {data[0].category}</span>
+                                        </p>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            {data[4].title}
+                                        </Typography>
+                                        <Typography gutterBottom >
+                                            By {data[4].author.name} - {data[4].author.published_date}
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            {data[4].details.length > 200 ? data[4].details.slice(0, 300) + "..." : data[0].details}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </Link>
                     </Grid>
                     <Grid item xs={6}>
-                    <Link href={`${data[5].category}/${data[5]._id}`}>
-              <Card >
-                    <CardActionArea>
-                        <CardMedia>
-                            <Image src={data[5].thumbnail_url} alt='news' width={800} height={500} />
-                        </CardMedia>
-                        <CardContent>
-                            <p>
-                                <span className=' bg-red-500 rounded  py-1 px-2 text-white'> {data[0].category}</span>
-                            </p>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {data[5].title}
-                            </Typography>
-                            <Typography gutterBottom >
-                            By {data[5].author.name} - {data[5].author.published_date}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                {data[5].details.length>200? data[0].details.slice(0,300)+"..." : data[0].details}
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-              </Link>
+                        <Link href={`${data[5].category}/${data[5]._id}`}>
+                            <Card >
+                                <CardActionArea>
+                                    <CardMedia>
+                                        <Image src={data[5].thumbnail_url} alt='news' width={800} height={500} />
+                                    </CardMedia>
+                                    <CardContent>
+                                        <p>
+                                            <span className=' bg-red-500 rounded  py-1 px-2 text-white'> {data[0].category}</span>
+                                        </p>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            {data[5].title}
+                                        </Typography>
+                                        <Typography gutterBottom >
+                                            By {data[5].author.name} - {data[5].author.published_date}
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            {data[5].details.length > 200 ? data[0].details.slice(0, 300) + "..." : data[0].details}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </Link>
                     </Grid>
 
                 </Grid>
